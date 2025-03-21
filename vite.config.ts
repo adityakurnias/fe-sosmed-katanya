b11a38baa4062a78920701a563f3c7f14d675736
adityakurnias <adityakurniasaputra903@gmail.com>
2025-03-21 19:46:27 +0700

    update api

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'https://socmed.synchronizeteams.my.id/api/v1',
